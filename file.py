43674e2a39fe9267a1b82303ef2a414e6bfc2579
chandra20500 <50678178+chandra20500@users.noreply.github.com>
2020-06-23 19:22:33 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,36 +1,47 @@
 import requests
+url = 'https://www.flipkart.com/amp/watches/fastrack~brand/pr?otracker=nmenu_sub_Men_0_Fastrack&sid=r18&start='
 import pandas as pd
+
+links = []
+
+for i in range(11):
+    links.append(url + str(60*i))
+    
+
+r = requests.get(url)
+
+pr = []
+pn = []
+pp = []
+
 from bs4 import BeautifulSoup
-url = 'https://www.flipkart.com/amp/watches/fastrack~brand/pr?otracker=nmenu_sub_Men_0_Fastrack&sid=r18&start=360'
 
-p_name = []
-p_rating = []
-p_price = []
-
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
-name = soup.find_all("p", {"class":"_2qAcZ"})
-for item in name:
-    p_name.append(item.string)
-
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
-price = soup.find_all("p", {"class":"_2lKKI"})
-for item in price:
-    p_price.append(item.string)
+for link in links:
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content)
+    data = soup.find_all("div", {"class":"_1jJGh"})
+    for item in data:
+        name = item.find_all('p', {"class":"_2qAcZ"})
+        rating = item.find_all('div', {"class":"_1sd66"})
+        price = item.find_all('p', {"class":"_2lKKI"})
+        if len(rating) == 0:
+            pr.append(0)
+        for j in rating:
+            pr.append(j.text)
+        if len(price) == 0:
+            pp.append(0)
+        for j in price:
+            pp.append(j.text)
+        if len(name) == 0:
+            pn.append(0)
+        for j in name:
+            pn.append(j.text)
         
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
-rating = soup.find_all("div", {"class":"_1sd66"})
-for item in rating:
-    p_rating.append(item.string)
 
-df2 = pd.DataFrame(p_price)
-df1 = pd.DataFrame(p_rating)
-df0 = pd.DataFrame(p_name)
+df1 = pd.DataFrame(pr)
+df2 = pd.DataFrame(pp)
+df3 = pd.DataFrame(pn)
+        
+result = pd.concat([df1,df2,df3], axis=1, sort=False)
 
-result = pd.concat([df0,df1,df2], axis=1, sort=False)
-
-print(result)
-
-result.to_csv('flipkart.csv') 
+result.to_csv("phones.csv")
